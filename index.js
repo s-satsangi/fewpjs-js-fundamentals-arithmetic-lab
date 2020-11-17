89717cdceb329daaf809030de1e99c74ae9b308f
s-satsangi <s.satsangi@gmail.com>
2020-11-16 21:29:44 -0500

    piece of cake

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+//ok
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random()*10)+1
+
+let mod = 24 % 5
+
+let max = Math.max(20,4)
